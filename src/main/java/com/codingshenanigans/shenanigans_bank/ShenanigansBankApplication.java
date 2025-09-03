b98ed9b58850b5d0093956da5c1f47e365829275
coding-shenanigans <coding.shenanigans@gmail.com>
2025-09-02 22:33:45 -0700

    Initial commit from Spring Initializr

---
+++ src/main/java/com/codingshenanigans/shenanigans_bank/ShenanigansBankApplication.java
@@ -0,0 +1,13 @@
+package com.codingshenanigans.shenanigans_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShenanigansBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShenanigansBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingshenanigans/shenanigans_bank/ShenanigansBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingshenanigans.shenanigans_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShenanigansBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
