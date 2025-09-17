8255d2552b778d87ecf7fa1bbaa475c5f5005ca8
Coding Shenanigans <94819102+coding-shenanigans@users.noreply.github.com>
2025-09-17 00:35:49 -0700

    Refactor token generation (#11)

--- src/main/java/com/codingshenanigans/shenanigans_bank/utils/Constants.java
+++ src/main/java/com/codingshenanigans/shenanigans_bank/utils/Constants.java
@@ -1,6 +1,7 @@
 package com.codingshenanigans.shenanigans_bank.utils;
 
 public class Constants {
-    public static final String ACCESS = "access";
-    public static final String REFRESH = "refresh";
+    public static final String ACCESS_TOKEN_KEY_ID = "access";
+    public static final String REFRESH_TOKEN_KEY_ID = "refresh";
+    public static final String REFRESH_TOKEN_COOKIE_NAME = "refresh_token";
 }
