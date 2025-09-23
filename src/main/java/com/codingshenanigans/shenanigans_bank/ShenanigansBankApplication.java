e39881e13213bd898d19396277666bcc7ec1b518
Coding Shenanigans <94819102+coding-shenanigans@users.noreply.github.com>
2025-09-22 23:48:34 -0700

    Fix server url in openapi ui (#22)

--- src/main/java/com/codingshenanigans/shenanigans_bank/ShenanigansBankApplication.java
+++ src/main/java/com/codingshenanigans/shenanigans_bank/ShenanigansBankApplication.java
@@ -1,12 +1,16 @@
 package com.codingshenanigans.shenanigans_bank;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
+@OpenAPIDefinition(
+        info = @Info(title = "OpenAPI definition", version = "v0"),
+        servers = {@Server(url = "/", description = "Default server url")}
+)
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class ShenanigansBankApplication {
 	public static void main(String[] args) {
